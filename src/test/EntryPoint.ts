26412c296d765e0fdcfebdbbdf4c2d7916e7512d
Dreamer <50623030+iselo@users.noreply.github.com>
2023-03-07 15:15:31 +0200

    0.1.1 (#3)

--- src/test/EntryPoint.ts
+++ src/test/EntryPoint.ts
@@ -1,10 +1,6 @@
 import {CleanWayBuilder} from "@raccoons-co/cleanway";
 import YourTest from "./YourTest";
 
-try {
-    CleanWayBuilder.instance()
-        .assign(new YourTest())
-        .build();
-} catch(exception) {
-    console.log(exception);
-}
+CleanWayBuilder.instance()
+    .assign(new YourTest())
+    .build();
--- src/test/YourTest.ts
+++ src/test/YourTest.ts
@@ -1,17 +1,20 @@
-import {Test} from "@raccoons-co/cleanway";
+import {Test,Log} from "@raccoons-co/cleanway";
 
 export default class YourTest {
 
+    @Log
     @Test
     public nothing() {
         // But your assertions here.
     }
 
+    @Log
     @Test
     public else() {
         // More assertions.
     }
 
+    @Log
     @Test
     public matters() {
         // For your clean code.
