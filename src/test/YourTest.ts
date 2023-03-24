b221168612b545542ad48807cb367d54d7548f46
iselo <ok+github@raccoons.co>
2023-03-24 10:37:06 +0200

    Add static analysis

--- src/test/YourTest.ts
+++ src/test/YourTest.ts
@@ -1,22 +1,19 @@
-import {Immutable, Log, Test} from "@raccoons-co/cleanway";
+import {Immutable, Test} from "@raccoons-co/cleanway";
 import {assert} from "chai";
 
 @Immutable
 export default class YourTest {
 
-    @Log
     @Test
     public nothing() {
         assert.ok("But your assertions here.");
     }
 
-    @Log
     @Test
     public else() {
         assert.ok("More assertions.");
     }
 
-    @Log
     @Test
     public matters() {
         assert.ok("For your clean code.");
