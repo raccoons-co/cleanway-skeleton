4cc2f76923a70bb9cab8e807b81512757bde2a0e
iselo <ok+github@raccoons.co>
2023-06-30 21:51:04 +0300

    Update eslint rules

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,13 +1,19 @@
 module.exports = {
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+  ],
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint"],
   root: true,
   rules: {
+    "max-len": ["warn", 120],
+    "max-lines": ["warn", 92],
     "eol-last": ["warn", "always"],
+    "indent": ["warn", 4],
     "quotes": ["warn", "double"],
-      "max-len": ["warn", 120 ],
-      "max-lines": ["warn", 92 ]
+    "semi": "off",
+    "@typescript-eslint/semi": "warn",
   },
   reportUnusedDisableDirectives: true,
   noInlineConfig: false
